ca9b4cc29622019035286581bbb007834a76ff45
Minh Pham <minh21345@gmail.com>
2025-08-14 17:52:23 +0700

    added query handler

--- backend/server.go
+++ backend/server.go
@@ -10,11 +10,20 @@ func main() {
 	mux := http.NewServeMux()
 
 	// Router
-	mux.HandleFunc("GET /", exampleFunc)
+	mux.HandleFunc("GET /", exampleHandler)
+	mux.HandleFunc("GET /query", queryHandler)
 	
 	log.Fatal(http.ListenAndServe(":5000", mux))
 }
 
-func exampleFunc(w http.ResponseWriter, r *http.Request) {
+func exampleHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World")
+}
+
+func queryHandler(w http.ResponseWriter, r *http.Request) {
+	lat := r.URL.Query().Get("lat")
+	long := r.URL.Query().Get("long")
+	current := r.URL.Query().Get("current")
+
+	fmt.Fprintln(w, lat, long, current)
 }
