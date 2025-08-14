5ca92ef17434278060bfe765b4197026092e67df
Minh Pham <minh21345@gmail.com>
2025-08-14 20:39:47 +0700

    now load envvar

--- backend/server.go
+++ backend/server.go
@@ -2,17 +2,36 @@ package main
 
 import (
 	"aggregate-site/backend/internal/database"
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"path"
+
+	"github.com/joho/godotenv"
 )
 
+var db *sql.DB
+
 func main() {
+	// loading envvar
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file, using default values...")
+	}
+
+	dbFile, exists := os.LookupEnv("DB_FILE")
+	if !exists {
+		dbFile = path.Join("feed.db")
+	}
+
 	// Database
-	_, err := database.Open("feed.db")
+	db, err := database.Open(dbFile)
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer db.Close()
 
 	mux := http.NewServeMux()
 
